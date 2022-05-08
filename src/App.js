3270a20040cf0b0368856d6c66a70b532cf3a1c9
Mahmoud Mostafa <mahmoud309mostafa@gmail.com>
2022-05-08 20:27:15 +0200

    Add Project List Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Components/Dashboard/Dashboard";
 import Navbar from "./Components/layout/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </>
   );
 }
---
+++ src/Components/projects/ProjectList.js
@@ -0,0 +1,113 @@
+import { Box, Card, CardContent, Stack, Typography } from "@mui/material";
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <Stack spacing={3} direction="column">
+      <Box>
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              Shrimp and Chorizo Paella
+            </Typography>
+            <Typography variant="body2" color="text.secondary" gutterBottom>
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like.
+            </Typography>
+            <Typography variant="body2" color="#00ff">
+              2<sup>th</sup> Jun 2022
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      <Box>
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              Shrimp and Chorizo Paella
+            </Typography>
+            <Typography variant="body2" color="text.secondary" gutterBottom>
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like.
+            </Typography>
+            <Typography variant="body2" color="#00ff">
+              2<sup>th</sup> Jun 2022
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      <Box>
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              Shrimp and Chorizo Paella
+            </Typography>
+            <Typography variant="body2" color="text.secondary" gutterBottom>
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like.
+            </Typography>
+            <Typography variant="body2" color="#00ff">
+              2<sup>th</sup> Jun 2022
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      <Box>
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              Shrimp and Chorizo Paella
+            </Typography>
+            <Typography variant="body2" color="text.secondary" gutterBottom>
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like.
+            </Typography>
+            <Typography variant="body2" color="#00ff">
+              2<sup>th</sup> Jun 2022
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      <Box>
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              Shrimp and Chorizo Paella
+            </Typography>
+            <Typography variant="body2" color="text.secondary" gutterBottom>
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like.
+            </Typography>
+            <Typography variant="body2" color="#00ff">
+              2<sup>th</sup> Jun 2022
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      <Box>
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              Shrimp and Chorizo Paella
+            </Typography>
+            <Typography variant="body2" color="text.secondary" gutterBottom>
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like.
+            </Typography>
+            <Typography variant="body2" color="#00ff">
+              2<sup>th</sup> Jun 2022
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+    </Stack>
+  );
+};
+
+export default ProjectList;
